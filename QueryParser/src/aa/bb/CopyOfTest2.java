42fe9f06fc6969052750cffffba4941919011ff6
lee <lee2day@gmail.com>
2014-05-30 16:06:21 +0900

    master commit 03

---
+++ QueryParser/src/aa/bb/CopyOfTest2.java
@@ -0,0 +1,14 @@
+package aa.bb;
+
+public class CopyOfTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Git");
+		System.out.println("Hello, Git");
+		System.out.println("Hello, Git");
+		System.out.println("Hello, Git");
+		System.out.println("Hello, Git");
+	}
+
+}
